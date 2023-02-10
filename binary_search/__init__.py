5b49fab94785a4c2771db96b95e2aa3f90522ff9
Shikha-code36 <pandeyshikha075@gmail.com>
2023-02-10 18:09:06 +0530

    init modified

--- binary_search/__init__.py
+++ binary_search/__init__.py
@@ -1,6 +1,11 @@
 # __init__.py
+from binary_search import binary_search
 
 def initialize():
+    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    target = 5
+    result = binary_search(arr, target)
+    print(result) 
     print("binary_search package initialized")
 
 initialize()
