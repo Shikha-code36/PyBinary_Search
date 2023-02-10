7f906849c946dc1c822603067fa446b252d4bb02
Shikha-code36 <pandeyshikha075@gmail.com>
2023-02-10 18:23:58 +0530

    restructure

--- pybinary_search/__init__.py
+++ pybinary_search/__init__.py
@@ -1,11 +1,13 @@
 # __init__.py
-from binary_search import binary_search
+from pybinary_search import binary_search
+
 
 def initialize():
     arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     target = 5
     result = binary_search(arr, target)
-    print(result) 
+    print(result)
     print("binary_search package initialized")
+
 
 initialize()
