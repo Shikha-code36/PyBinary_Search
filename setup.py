5649c821d109f3f4f13ecad3557e6f4dca2bf364
Shikha-code36 <pandeyshikha075@gmail.com>
2023-02-10 17:26:28 +0530

    Binary Search

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='binary-search',
+    version='0.0.1',
+    description='A binary search implementation in Python',
+    author='Shikha Pandey',
+    author_email='shikha.py36@gmail.com',
+    url='https://github.com/Shikha-code36/PyBinary_Search',
+    packages=['binary_search'],
+    install_requires=[]
+)
